8fcf5bc4c999d558202c91da6d96836ca228edf1
KrPrince19 <princekumarydv19@gmail.com>
2025-10-02 23:29:48 +0530

    Update page.js

--- bgmiscrimapp/app/profile/page.js
+++ bgmiscrimapp/app/profile/page.js
@@ -1,34 +1,4 @@
-// import React from 'react'
-// import { currentUser } from '@clerk/nextjs/server'
-// import { UserButton } from '@clerk/nextjs'
- 
-// const page = async () => {
-//    const user = await currentUser()
-   
-//   return (
-//    <div className=" w-[70vw] min-h-screen flex items-start justify-center -ml-10 mt-20 py-8">
-//   <div className="bg-white text-black shadow-lg rounded-lg w-full max-w-sm sm:max-w-md lg:w-90  flex flex-col items-center">
-//     <UserButton />
-//     <p className="text-center mb-4">Hi, {user?.username} — Welcome to FragZone</p>
-//     <p className="text-center mb-4">{user?.emailAddresses[0].emailAddress}</p>
-    
 
-
-//   <h1 className='text-red-300'>
-//   {process.env.NEXT_PUBLIC_MATCH_PASSWORD === "" ? (
-//     <span>Password will show before 15 minutes of match time.</span>
-//   ) : (
-//     <span>Password Is:-{process.env.NEXT_PUBLIC_MATCH_PASSWORD}</span>
-//   )}
-// </h1>
-
-// </div>
-//   </div>
-
-//   )
-// }
-
-// export default page
 
 import { currentUser } from "@clerk/nextjs/server";
 import UserDashboard from "./UserDashboard";
