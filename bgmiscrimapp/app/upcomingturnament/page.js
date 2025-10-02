e2dcf174f5e1ee2619c31abc46c7d84282c53fcc
KrPrince19 <princekumarydv19@gmail.com>
2025-10-02 23:32:13 +0530

    Update page.js

--- bgmiscrimapp/app/upcomingturnament/page.js
+++ bgmiscrimapp/app/upcomingturnament/page.js
@@ -32,70 +32,7 @@ const Page = () => {
   
   return (
     <>
-     {/* <div className='turnament  flex gap-44  cursor-pointer '>
-        <div>
-          <h1
-            onClick={() => setSelected("two")}
-            className={`-mt-4 font-bold transition-colors ${
-              selected === "two" ? "text-yellow-500 underline": "text-black"
-            }`}
-          >
-             <Link href="/turnament" >
-            ONGOING TURNAMENT
-            </Link>
-          </h1>
-        </div>
-        <div>
-          <h1
-            onClick={() => setSelected("one")}
-            className={`-mt-4 font-bold transition-colors ${
-              selected === "one" ? "text-yellow-500 underline" : "text-black"
-            }`}
-          >
-            <Link href="/upcomingturnament" >
-            UPCOMING TURNAMENT
-            </Link>
-          </h1>
-        </div>
-      </div>
- {loading ? (
-        <p className="text-black">Loading tournaments...</p>
-      ) : (
-    <div className="container flex flex-col w-full h-[80vh] gap-3.5 py-4 ">
-      {upcomingTournaments.map((tournament, idx) => (
-        <div
-          key={idx}
-          className="card w-full bg-gray-900 p-2 -mt-2 h-auto flex justify-between items-start rounded-t-2xl border-r-1 border-l-1 border-yellow-200"
-        >
-          <div className="left mx-22">
-           <Link  className="text-black" href={`/details/${tournament.id}`}>
-               <div className="card-title text-black text-twire-light hover:underline hover:text-cyan-400 font-sans font-bold text-2xl mb-4"> 
-                {tournament.name.toUpperCase()}
-               </div>
-              </Link>
-            <div className="start-date flex gap-10 pt-4 text-amber-300 mb-2">
-              <div>
-                <h1 className="text-sm font-semibold ">START DATE:-</h1>
-                <span className="text-black">{tournament.startdate}</span>
-              </div>
-              <div>
-                <h1 className="text-sm font-semibold">END DATE:-</h1>
-                <span className="text-black">{tournament.enddate}</span>
-              </div>
-            </div>
-          </div>
-          <div className="right">
-            <div className="detail  mt-12 ">
-              <Link  className="text-black" href={`/details/${tournament.id}`}>
-            <button className="text-black  hover:underline  hover:text-cyan-400">DETAILS</button>
-          </Link>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-      )} */}
-
+  
        <div className=' -ml-[71px] flex flex-col items-center  min-h-screen py-22 text-black '>
       {/* This div represents the content area that would be passed as 'children' */}
       <div className="  sm:mx-68">
