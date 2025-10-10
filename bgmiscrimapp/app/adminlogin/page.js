d7793462aa29a92bd4b04b37495e3d3fda3699e8
KrPrince19 <princekumarydv19@gmail.com>
2025-10-10 23:26:13 +0530

    Update page.js

--- bgmiscrimapp/app/adminlogin/page.js
+++ bgmiscrimapp/app/adminlogin/page.js
@@ -17,7 +17,7 @@ export default function AdminLogin() {
     setError('');
 
     try {
-      const res = await fetch('http://localhost:5000/adminlogin', {
+      const res = await fetch(`${process.env.URL}/adminlogin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
