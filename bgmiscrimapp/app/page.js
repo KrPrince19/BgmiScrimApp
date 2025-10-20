3606be947717483acb3ea95270e29cd2fec7e918
KrPrince19 <princekumarydv19@gmail.com>
2025-10-20 12:37:43 +0530

    Update page.js
    change

--- bgmiscrimapp/app/page.js
+++ bgmiscrimapp/app/page.js
@@ -1,9 +1,6 @@
 // app/page.js
 import HomePage from './home/page';
 
-// Revalidate page every 60 seconds (like getStaticProps revalidate)
-export const revalidate = 60;
-
 export default function Page() {
   return <HomePage />;
 }
